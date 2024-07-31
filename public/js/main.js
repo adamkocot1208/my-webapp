0fb4cb85d8f82834e8a9b35ba46f9afe90704e2b
adamkocot1208 <adam.kocot1208@gmail.com>
2024-07-31 10:20:51 +0200

    dodanie nowych elementow

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+    }).addTo(map);
+
+    // Add additional Leaflet functionalities...
+});
